114471b0c5ad543a28126e40dac471ae222cfe21
Josh <j.webb10215@gmail.com>
2022-05-16 10:16:04 +1200

    Implement encoder handler

--- simple_encoder.cpp
+++ simple_encoder.cpp
@@ -10,11 +10,83 @@
  **/
 
 #include "simple_encoder.h"
+#include "PinChangeInterrupt.h"     // nicohood/PinChangeInterrupt@^1.2.9
+#include <Arduino.h>
 
-Encoder::Encoder(uint8_t A_pin, uint8_t B_pin) {
-
+Encoder::Encoder(uint8_t A_PIN, uint8_t B_PIN) {
+    attachPCINT(digitalPinToPCINT(A_PIN), encoder_int_handler_A, CHANGE);
+    attachPCINT(digitalPinToPCINT(B_PIN), encoder_int_handler_B, CHANGE);
+    A_pin = A_PIN;
+    B_pin = B_PIN;
 }
 
-Encoder::Encoder(uint8_t A_pin, uint8_t B_pin, uint8_t SW_pin) {
-    
+Encoder::Encoder(uint8_t A_PIN, uint8_t B_PIN, uint8_t SW_PIN) {
+    attachPCINT(digitalPinToPCINT(A_PIN), encoder_int_handler_A, CHANGE);
+    attachPCINT(digitalPinToPCINT(B_PIN), encoder_int_handler_B, CHANGE);
+    A_pin = A_PIN;
+    B_pin = B_PIN;
+    SW_pin = SW_PIN;
+    pinMode(SW_PIN, INPUT);
+    hasSwitch = 1;
+}
+
+void Encoder::encoder_int_handler_A(void) {
+    // Read the pin states
+    A = digitalRead(A_pin);
+    B = digitalRead(B_pin);
+
+    if (ISR_cooldown_ready()) {
+        // Phase A
+        if (A) {
+            if (B) {
+                enc_pos--;            // CCW
+            } else {
+                enc_pos++;            // CW
+            }
+        } else {
+            if (B) {
+                enc_pos++;            // CW
+            } else {
+                enc_pos--;            // CCW
+            }
+        }
+    }
+}
+
+void Encoder::encoder_int_handler_B(void) {
+    // Read the pin states
+    A = digitalRead(A_pin);
+    B = digitalRead(B_pin);
+
+    if (ISR_cooldown_ready()) {
+        // Phase B
+        if (B) {
+            if (A) {
+                enc_pos++;            // CW
+            } else {
+                enc_pos--;            // CCW
+            }
+        } else {
+            if (A) {
+                enc_pos--;            // CCW
+            } else {
+                enc_pos++;            // CW
+            }
+        }
+    }
+}
+
+uint8_t Encoder::read_switch(void) {
+    SW = digitalRead(SW_pin);
+    return SW;
+}
+
+uint8_t Encoder::ISR_cooldown_ready(void) {
+    uint8_t ret = 0;
+    uint32_t t = millis();
+    if ((t - ISR_timestamp) > ISR_period) {
+        ret = 1;
+    }
+    ISR_timestamp = t;
+    return ret;
 }
--- simple_encoder.h
+++ simple_encoder.h
@@ -15,17 +15,25 @@
 
 class Encoder {
     public:
-        Encoder(uint8_t A_pin, uint8_t B_pin);
-        Encoder(uint8_t A_pin, uint8_t B_pin, uint8_t SW_pin);
+        Encoder(uint8_t A_PIN, uint8_t B_PIN);
+        Encoder(uint8_t A_PIN, uint8_t B_PIN, uint8_t SW_PIN);
+        uint8_t read_switch(void);
 
     private:
         uint8_t hasSwitch = 0;
         uint8_t A_pin;
         uint8_t B_pin;
         uint8_t SW_pin;
-        volatile uint16_t A;
-        volatile uint16_t B;
-        volatile uint8_t SW;
+        uint8_t SW;
+        volatile uint8_t A;
+        volatile uint8_t B;
+        volatile int32_t enc_pos = 0;
+        volatile uint32_t ISR_timestamp = 0;
+        const uint16_t ISR_period = 100;        // ms
+
+        void encoder_int_handler_A(void);
+        void encoder_int_handler_B(void);
+        uint8_t ISR_cooldown_ready(void);
 };
 
 #endif
